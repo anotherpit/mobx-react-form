4b90e9e29ebcb305a46182e528b2dea9eac7143b
Claudio Savino <claudio.savino@me.com>
2016-11-08 22:27:40 +0100

    fix

--- src/umd.js
+++ src/umd.js
@@ -1,12 +1,14 @@
 /* eslint no-console: 0 */
 /* eslint no-undef: 0 */
 
-console.log('MobxReactForm', MobxReactForm);
+// console.log('MobxReactForm', MobxReactForm);
 
 const form = new MobxReactForm({
-  email: 'test@test.com',
+  fields: {
+    email: 'test@test.com',
+  },
 });
 
-console.log('form', form);
-console.log('form.get()', form.get('values'));
-console.log('form.set()', form.set({ email: 'test' }));
+// form.update({ email: 'test' });
+
+console.log('form.values()', form.values());
